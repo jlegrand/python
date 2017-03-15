f516b7ac663dbcf7e68b9c02c7cabee104216438
jlegrand <julien.legrand@gmail.com>
2017-03-15 15:37:57 +0100

    decorator de classe avec argument

--- basics/decorator_de_classe.py
+++ basics/decorator_de_classe.py
@@ -1,16 +1,23 @@
 class Decorator:
+    def __init__(self, arg):
+        self.arg = arg
+
     def __call__(self, cls):
-        class enveloppe(cls):
-            def multiplier(self, value):
-                return value * 2
-        return enveloppe
+        arg = self.arg
+
+        class Enveloppe(cls):
+            @staticmethod
+            def multiplier(value):
+                return value * arg
+        return Enveloppe
 
 
+@Decorator(4)
 class TestClass:
     def multiplier(self, value):
         return value * 3
 
-TestClass = Decorator()(TestClass)
+# TestClass = Decorator(4)(TestClass)
 if __name__ == '__main__':
     to = TestClass()
     print(to.multiplier(5))
