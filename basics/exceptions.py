71349e7287826c74785ed43ae8bcb861859f8432
jlegrand <julien.legrand@gmail.com>
2017-03-15 16:11:36 +0100

    Basics de gestion des exceptions

---
+++ basics/exceptions.py
@@ -0,0 +1,16 @@
+num = 1
+
+try:
+    print(num/den)
+# except NameError:
+#     print('variable non definie')
+# except (TypeError, ZeroDivisionError):
+#     print('erreur type ou zero')
+except Exception as e:
+    print('type : ', type(e).__name__)
+    print('message : ', e)
+    print(repr(e))
+else:
+    print('else')
+finally:
+    print('finally')
