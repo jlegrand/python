14fd551907ca604695e906a99efd351496d7d303
jlegrand <julien.legrand@gmail.com>
2017-03-15 13:52:36 +0100

    Application utilisant un decorateur

--- basics/decorator.py
+++ basics/decorator.py
@@ -1,12 +1,12 @@
 def decorator(f):
-    print('debut decorateur')
+    # print('debut decorateur')
 
     def f_out(arg):
         print('pre traitement')
         out = f(arg)
         print('post traitement')
         return out
-    print('fin decorateur')
+    # print('fin decorateur')
     return f_out
 
 
@@ -15,4 +15,24 @@ def ma_fonction(arg):
     return('la fonction', arg)
 
 
-print(ma_fonction(ma_fonction.__name__))
+# print(ma_fonction(ma_fonction.__name__))
+
+
+####
+def convert(f):
+    def f_convert(x, y):
+        return f(int(x), int(y))
+    return f_convert
+
+@convert
+def somme(x, y):
+    return x+y
+
+@convert
+def produit(x, y):
+    return x*y
+
+print(somme(1.2, 3.2))  # => doit retourner 4
+print(produit(1.2, 3.2))  # => doit retourner 3
+
+###
