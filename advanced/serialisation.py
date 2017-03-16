d3886613a147c0fdc93550ef4cf4e800908da1ef
jlegrand <julien.legrand@gmail.com>
2017-03-16 11:23:10 +0100

    multi serialisation

--- advanced/serialisation.py
+++ advanced/serialisation.py
@@ -2,6 +2,9 @@ from personne import Personne
 import pickle
 
 moi = Personne('Legrand', 'Julien', 8, 12, 1980)
+toi = Personne('Legrand', 'Julie', 9, 12, 1980)
 
 with open('serial.p', 'wb') as fichier:
+    pickle.dump(moi, fichier)
+    pickle.dump(toi, fichier)
     pickle.dump(moi, fichier)
